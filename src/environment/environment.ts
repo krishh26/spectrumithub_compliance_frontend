fda30f7ace9c9a360d845eb71855b514e0d7948f
krishh26 <kishansinhparmar999@gmail.com>
2025-04-04 18:34:23 +0530

    end point change

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
 
   // baseUrl: "http://localhost:3006",
   // baseUrl : 'http://192.168.1.7:4100' ,
-  baseUrl: 'https://api.delphiservices.co.uk'
+  baseUrl: 'https://api.spectrumithub.com'
 };
 
 
